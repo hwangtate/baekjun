f09b0b8fc6cb853c7632d8c20803ecf6ce25f264
hwangtate <ghkdxodud10253504@gmail.com>
2024-07-22 21:17:53 +0900

    9단계 문제

--- 1차원 배열/8단계_3052.py
+++ 1차원 배열/8단계_3052.py
@@ -9,3 +9,15 @@ clone_result = result
 
 print(len(result & clone_result))
 
+#set(remainders)는 리스트 remainders를 집합으로 변환합니다. 집합은 중복된 요소를 허용하지 않으므로, 이 과정에서 중복된 나머지 값이 제거됩니다.
+#그렇기 떄문에... 아래 코드로 최적화 가능합니다...
+
+# # 10개의 숫자를 입력받아 리스트로 저장
+# numbers = [int(input()) for _ in range(10)]
+#
+# # 나머지 리스트 생성
+# remainders = [num % 42 for num in numbers]
+#
+# # 서로 다른 나머지 값의 개수 출력
+# unique_remainders = len(set(remainders))
+# print(unique_remainders)
